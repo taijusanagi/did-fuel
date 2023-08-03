df6da2f1f9811473240433cafae570e0dec1b8a6
taijusanagi <taijusanagi@gmail.com>
2023-08-03 16:25:13 +0700

    feat: done with frontend

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,118 +1,97 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+interface VerificationMethod {
+  id: string;
+  type: string;
+  controller: string;
+}
+
+const Home: React.FC = () => {
+  const [did, setDid] = useState<string>("did:fuel:xxx");
+  const [verificationMethods, setVerificationMethods] = useState<VerificationMethod[]>([
+    {
+      id: `${did}`,
+      type: "EcdsaSecp256k1RecoveryMethod2020",
+      controller: did,
+    },
+  ]);
+  const [newKeyInput, setNewKeyInput] = useState<string>("");
+  const [didDocument, setDidDocument] = useState<Record<string, any>>({
+    "@context": ["https://www.w3.org/ns/did/v1", "https://w3id.org/security/suites/secp256k1recovery-2020/v2"],
+    id: did,
+    verificationMethod: verificationMethods,
+    authentication: [`${did}#controller`],
+    assertionMethod: [`${did}#controller`],
+  });
+
+  useEffect(() => {
+    setDidDocument({
+      ...didDocument,
+      id: did,
+      verificationMethod: verificationMethods,
+      authentication: [`${did}#controller`],
+      assertionMethod: [`${did}#controller`],
+    });
+  }, [did, verificationMethods]);
+
+  const handleAddKey = () => {
+    const newKey: VerificationMethod = {
+      id: `did:fuel:${newKeyInput}`,
+      type: "EcdsaSecp256k1RecoveryMethod2020",
+      controller: did,
+    };
+    setVerificationMethods([...verificationMethods, newKey]);
+    setNewKeyInput("");
+  };
+
+  const handleRevokeKey = (id: string) => {
+    const newVerificationMethods = verificationMethods.filter((method) => method.id !== id);
+    setVerificationMethods(newVerificationMethods);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className={`flex min-h-screen flex-col items-center p-24 mx-auto max-w-4xl ${inter.className}`}>
+      <div className="mb-12 text-center text-5xl font-bold min-w-full">DID:Fuel</div>
+      <div className="p-4 max-w-4xl min-w-full bg-white rounded-lg">
+        <h1 className="mb-4 text-lg font-bold min-w-full text-black">{did}</h1>
+        <h2 className="mb-2 text-sm font-bold text-black">DID Document:</h2>
+        <pre className="mb-4 border rounded p-4 overflow-x-scroll min-w-full bg-gray-100 shadow-inner">
+          {JSON.stringify(didDocument, null, 2)}
+        </pre>
+        <h2 className="mb-2 text-sm font-bold text-black">Management</h2>
+        <input
+          className="mb-4 px-2 py-2 rounded-lg min-w-full border border-gray-300"
+          placeholder="Fuel account"
+          value={newKeyInput}
+          onChange={(e) => setNewKeyInput(e.target.value)}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg min-w-full disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={handleAddKey}
+          disabled={!newKeyInput}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Delegate
+        </button>
+        <ul className="mt-4 min-w-full">
+          {verificationMethods.map(({ id }) => (
+            <li className="flex justify-between items-center p-2 my-2 min-w-full bg-white rounded-lg" key={id}>
+              <span className="text-black">{id}</span>
+              <button
+                className="px-2 py-1 bg-red-500 text-white rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
+                onClick={() => handleRevokeKey(id)}
+                disabled={did === id}
+              >
+                Revoke
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
